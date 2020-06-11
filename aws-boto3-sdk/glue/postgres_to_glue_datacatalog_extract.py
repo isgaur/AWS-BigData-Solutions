2e7b9d62d503df61b4b0f17e92b8f78c7aec4e76
Ishan <62407228+isgaur@users.noreply.github.com>
2020-06-10 21:50:36 -0700

    Update postgres_to_glue_datacatalog_extract.py

--- aws-boto3-sdk/glue/postgres_to_glue_datacatalog_extract.py
+++ aws-boto3-sdk/glue/postgres_to_glue_datacatalog_extract.py
@@ -1,16 +1,15 @@
 /**
 
-1> launched a Postgres sever and created the table using following query: 
-========>
+1> launched a Postgres sever and created the table using following query:
+  
 CREATE TABLE ishan ("userID" VARCHAR (50) PRIMARY KEY, "UserName" VARCHAR (50));
-========>
 
 2> Ran crawler on the Postgres sever which will create the table.
 
 3> Create Glue ETL job by select source table as Postgres table "ishan" and target source as S3 location.
 
 4> creata a glue pyspark job with the following code: 
-==========>
+
 **/
 
 import sys
@@ -32,12 +31,10 @@ job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 sqlContext = SQLContext(sc)
 sqlContext.setConf("spark.sql.caseSensitive","true")
-##print "Startdate:  ", args['start_date']
-#print("query to mysql is: "+query)
+
 
 datasource0 = sqlContext.read.format("jdbc").option("url", "jdbc:postgresql://<postgres endpoint>:5432/postgres").option("query","select * from ishan").option("user", "postgres").option("password", "password").load()
 datasource12=datasource0.repartition(1)                       
 datasource1=DynamicFrame.fromDF(datasource12,glueContext,"test")
 datasink2 = glueContext.write_dynamic_frame.from_options(frame = datasource1, connection_type = "s3", connection_options = {"path": "s3://<bucket-name>/images/"}, format = "csv", transformation_ctx = "datasink2")
 job.commit()
-==========>
