0861995a6bb89bfb81b072c5337b41cc20bc2fd6
Ishan <62407228+isgaur@users.noreply.github.com>
2021-01-15 23:46:07 -0800

    Update spark_submit.sh

--- EMR/spark_submit.sh
+++ EMR/spark_submit.sh
@@ -6,3 +6,8 @@ spark-submit --master yarn --deploy-mode cluster --executor-memory 4g --conf spa
 
 The number of cores is entirely depends on the parameter spark.executor.cores or --executor-cores . As long one defines it in the /etc/spark/conf/spark-defaults.conf  it will pick up the default value 
 from this file or if you want to change this value per application , please pass it using spark-submit as mentioned above.
+
+
+Starting PySpark-shell interactively with more memory etc.
+
+pyspark --executor-cores 5 --executor-memory 36g --driver-memory 36g --driver-cores 5
