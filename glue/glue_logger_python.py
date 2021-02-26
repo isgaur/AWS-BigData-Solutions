d2e5f86d39baa18a28f20bc34f1b5a8f9361b4c0
Ishan <62407228+isgaur@users.noreply.github.com>
2021-02-26 10:37:45 -0800

    Update glue_logger_python.py

--- glue/glue_logger_python.py
+++ glue/glue_logger_python.py
@@ -5,7 +5,10 @@ from pyspark.context import SparkContext
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
+
+# Continuous logging must be Enabled in the job definition for this logging
 logger = glueContext.get_logger()
+
 logger.info("info message")
 logger.warn("warn message")
 logger.error("error message")
